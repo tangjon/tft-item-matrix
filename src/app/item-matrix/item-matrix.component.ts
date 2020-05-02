0468f641d7c5b82a55dfe5bc3afb3a0babf84aff
Jonathan <tangjon@hotmail.com>
2020-05-02 12:31:20 -0700

    stable

--- src/app/item-matrix/item-matrix.component.ts
+++ src/app/item-matrix/item-matrix.component.ts
@@ -24,11 +24,15 @@ export class ItemMatrixComponent implements OnInit {
   MATRIX_DIM = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   MIDDLE_MOUSE_CLICK = 2;
+  RIGHT_CLICK = 3;
 
   STYLE_HAS = 'green';
   MATRIX = [];
 
   ASSETS_FOLDER = 'assets/images/set3/';
+
+  highlight_col_num = -1;
+  highlight_row_num = -1;
 
   constructor() {
     // set inner matrix
@@ -57,98 +61,108 @@ export class ItemMatrixComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onMouseOver(event: MouseEvent): void {
-    const el = event.target as HTMLElement;
-    const row = el.getAttribute('row');
-    const col = el.getAttribute('col');
-    el.style.backgroundColor = 'red';
-    // $(`#${row}-${col}`).css('backgroundColor', 'red');
 
-    $(`#0-${row}`).css('backgroundColor', 'green');
-    $(`#${col}-0`).css('backgroundColor', 'green');
-
-  }
-
-  onMouseLeave(event: MouseEvent): void {
-    const el = event.target as HTMLElement;
-    const row = el.getAttribute('row');
-    const col = el.getAttribute('col');
-
-    if (!(this.MATRIX[col][row].count > 0)) {
-      el.style.backgroundColor = 'white';
-    }
-
-    if (!(this.MATRIX[col][0].count > 0)) {
-      $(`#${col}-0`).css('backgroundColor', 'white');
-    }
-
-    if (!(this.MATRIX[0][row].count > 0)) {
-      $(`#0-${row}`).css('backgroundColor', 'white');
-    }
+  onMouseLeave(): void {
+    this.highlight_col_num = -1;
+    this.highlight_row_num = -1;
   }
 
 
-  onMouseClick(event: MouseEvent) {
-    console.log(event);
-    const el = event.target as HTMLElement;
-    const row = el.getAttribute('row');
-    const col = el.getAttribute('col');
-
-    if (row === '0' || col === '0') {
-      this.MATRIX[col][row].count += 1;
-      this.MATRIX[row][col].count += 1;
+  markAsWant($event, col, row) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    // mark item piece
+    if (col === row) {
+      this.MATRIX[col][row].wanted = !this.MATRIX[col][row].wanted;
+    } else {
+      this.MATRIX[row][col].wanted = !this.MATRIX[col][row].wanted;
+      this.MATRIX[col][row].wanted = !this.MATRIX[col][row].wanted;
     }
 
+    // if (col === 0 || row === 0) {
+    //   this.MATRIX[row][col].wanted = !this.MATRIX[col][row].wanted;
+    //   this.MATRIX[col][row].wanted = !this.MATRIX[col][row].wanted;
+    // } else {
+    //   // mark item
+    //
+    //   // if any is marked, mark all
+    //   if (this.MATRIX[col][row].wanted === true || this.MATRIX[0][row].wanted === true || this.MATRIX[col][0].wanted === true) {
+    //     this.MATRIX[col][row].wanted = true;
+    //     this.MATRIX[0][row].wanted = true;
+    //     this.MATRIX[col][0].wanted = true;
+    //   }
+    //   // if all is marked, unmark all
+    //   else if (this.MATRIX[col][row].wanted === true && this.MATRIX[0][row].wanted === true && this.MATRIX[col][0].wanted === true) {
+    //     this.MATRIX[col][row].wanted = false;
+    //     this.MATRIX[0][row].wanted = false;
+    //     this.MATRIX[col][0].wanted = false;
+    //   } else {
+    //     // none is marked, mark all
+    //     this.MATRIX[col][row].wanted = true;
+    //     this.MATRIX[0][row].wanted = true;
+    //     this.MATRIX[col][0].wanted = true;
+    //   }
+    //
+    //
+    // }
 
-    const itemColCount = this.MATRIX[col][0].count;
-    const itemRowCount = this.MATRIX[0][row].count;
-    if (itemColCount > 0 && itemRowCount > 0) {
-      this.buyItem(col, row);
-    }
   }
 
-  onRightClick(event: MouseEvent) {
-    event.preventDefault();
-    const el = event.target as HTMLElement;
-    const row = el.getAttribute('row');
-    const col = el.getAttribute('col');
-    this.MATRIX[col][row].count -= 1;
-  }
+
+  // ACTIONS
 
   buyItem(col, row) {
-    this.MATRIX[col][0].count -= 1;
-    this.MATRIX[0][row].count -= 1;
-  }
-
-  markAsWant(col, row) {
-    this.MATRIX[col][row].wanted = !this.MATRIX[col][row].wanted;
-  }
-
-  onMiddleMouseClick(event: MouseEvent) {
-    const el = event.target as HTMLElement;
-    const row = el.getAttribute('row');
-    const col = el.getAttribute('col');
-    if (event.which === this.MIDDLE_MOUSE_CLICK) {
-      this.markAsWant(col, row);
+    if (this.MATRIX[col][0].count > 0 && this.MATRIX[0][row].count > 0) {
+      this.MATRIX[col][0].count -= 1;
+      this.MATRIX[0][row].count -= 1;
     }
   }
 
-  onMouseLeaveMatrix($event: MouseEvent) {
-    for (let col = 1; col < this.MATRIX_DIM.length; col++) {
-      for (let row = 1; row < this.MATRIX_DIM.length; row++) {
-        if (this.MATRIX[0][row].count > 0 && this.MATRIX[col][0].count > 0) {
-          $(`#${col}-${row}`).css('backgroundColor', 'red');
-        }
-      }
-    }
+  // FLAGS
+
+  canBuy(col, row) {
+    return this.MATRIX[0][row].count > 0 && this.MATRIX[col][0].count > 0 && col <= row;
   }
 
-  onMouseEnterMatrix() {
-    for (let col = 1; col < this.MATRIX_DIM.length; col++) {
-      for (let row = 1; row < this.MATRIX_DIM.length; row++) {
-        $(`#${col}-${row}`).css('backgroundColor', 'white');
-      }
-    }
+
+  // MOUSE EVENTS
+
+  onMouseOver(col, row): void {
+    this.highlight_col_num = col;
+    this.highlight_row_num = row;
   }
 
+  onMouseClick(col, row) {
+
+    // add to count
+    if (col === 0 || row === 0) {
+      this.MATRIX[col][row].count += 1;
+      this.MATRIX[row][col].count += 1;
+    } else {
+      this.buyItem(col, row);
+    }
+
+    // const el = event.target as HTMLElement;
+    // const row = el.getAttribute('row');
+    // const col = el.getAttribute('col');
+    //
+    // if (row === '0' || col === '0') {
+    //   this.MATRIX[col][row].count += 1;
+    //   this.MATRIX[row][col].count += 1;
+    // }
+    //
+    //
+    // const itemColCount = this.MATRIX[col][0].count;
+    // const itemRowCount = this.MATRIX[0][row].count;
+    // if (itemColCount > 0 && itemRowCount > 0) {
+    //   this.buyItem(col, row);
+    // }
+  }
+
+  onOtherClick($event: MouseEvent, col: number, row: number) {
+    if ($event.which === this.RIGHT_CLICK) {
+      this.MATRIX[col][row].count -= 1;
+      this.MATRIX[row][col].count -= 1;
+    }
+  }
 }
